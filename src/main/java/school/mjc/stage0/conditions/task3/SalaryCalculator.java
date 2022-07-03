e7d919ec3f0e35376441b8e5a6ef502f0481e5b6
elzbieta-f <elzbieta.fulga@gmail.com>
2022-07-03 20:16:37 +0300

    Multiple conditions task submission

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary>0){
+            double afterTaxes;
+            if (salary<=10000){
+                afterTaxes=salary*0.85;
+            } else if (salary>20000) {
+                afterTaxes=salary*0.8;
+            } else {
+                afterTaxes=salary*0.82;
+            }
+            System.out.println(afterTaxes);
+
+        } else {
+            System.out.println("wrong input!");
+        }
     }
 }
